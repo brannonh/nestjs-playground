173a2f7d0d872495dc7b8ef761f1e50d1467b716
Brannon Hall <brannon.hall@mastery.net>
2021-12-15 12:43:25 -0600

    Added role guard.

---
+++ src/guards/role.guard.ts
@@ -0,0 +1,24 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean{
+    const roles = this.reflector.get<string[]>('roles', context.getHandler());
+    if (!roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const user = request.user;
+
+    return this.matchRoles(roles, user.roles);
+  }
+
+  matchRoles(requiredRoles: string[], userRoles: string[]): boolean {
+    return requiredRoles.some(role => userRoles.includes(role));
+  }
+}
--- src/pens/pens.controller.ts
+++ src/pens/pens.controller.ts
@@ -1,17 +1,21 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UseFilters, UsePipes } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, SetMetadata, UseFilters, UseGuards, UsePipes } from '@nestjs/common';
 import { CreatePenDto } from './dto/pens.dto';
 import { PensService } from './pens.service';
 import { PlaygroundExceptionFilter } from 'src/filters/playground-exception.filter';
 import { JoiValidationPipe } from 'src/pipes/joi-validation.pipe';
 import { CreatePenSchema } from './schemas/create-pen.schema';
 import { DtoValidationPipe } from 'src/pipes/dto-validation.pipe';
+import { RoleGuard } from 'src/guards/role.guard';
+import { Roles } from 'src/decorators/roles.decorator';
 
 @Controller('pens')
+@UseGuards(RoleGuard)
 @UseFilters(PlaygroundExceptionFilter)
 export class PensController {
   constructor(private pensService: PensService) {}
 
   @Post()
+  @Roles('admin')
   // @UsePipes(new JoiValidationPipe(CreatePenSchema))
   async create(@Body(new DtoValidationPipe()) createPenDto: CreatePenDto): Promise<string> {
     return 'This action adds a new pen.';
